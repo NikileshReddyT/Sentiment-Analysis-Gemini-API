883b4ad5ef3cbc50a93ae2a69667195de4284204
NikileshReddyT <nikileshreddyt@gmail.com>
2024-11-28 00:30:52 +0530

    changed the styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import ApiKeyPage from './components/ApiKeyPage'
 import AnalysisPage from './components/AnalysisPage'
+import Footer from './components/Footer'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -11,31 +12,34 @@ function App() {
 
   return (
     <Router>
-      <div className="app-container">
-        <Routes>
-          <Route 
-            path="/" 
-            element={
-              <ApiKeyPage 
-                apiKey={apiKey} 
-                setApiKey={setApiKey} 
-              />
-            } 
-          />
-          <Route 
-            path="/analysis" 
-            element={
-              apiKey ? (
-                <AnalysisPage 
+      <div className="min-h-screen flex flex-col">
+        <div className="flex-grow">
+          <Routes>
+            <Route 
+              path="/" 
+              element={
+                <ApiKeyPage 
                   apiKey={apiKey} 
-                  onLogout={clearApiKey}
+                  setApiKey={setApiKey} 
                 />
-              ) : (
-                <Navigate to="/" />
-              )
-            } 
-          />
-        </Routes>
+              } 
+            />
+            <Route 
+              path="/analysis" 
+              element={
+                apiKey ? (
+                  <AnalysisPage 
+                    apiKey={apiKey} 
+                    onLogout={clearApiKey}
+                  />
+                ) : (
+                  <Navigate to="/" />
+                )
+              } 
+            />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   )
